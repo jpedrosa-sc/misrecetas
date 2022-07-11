16a6dd4c7bdcbdd9013114b63e6276554339f947
jpedrosa-sc <104893038+jpedrosa-sc@users.noreply.github.com>
2022-07-11 11:14:30 +0200

    variables, params y contacto

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.query.color1);
+  console.log(req.query.color2);
+  console.log(req.query.color3);
   res.render('index');
 });
 
 /* GET contact page. */
 router.get('/contacto', function(req, res, next) {
-  res.send('Aquí irá mi página de contacto');
+  res.render('contacto');
+});
+
+/* GET contact page. */
+router.post('/validar-contacto', function(req, res, next) {
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log(email,password);
+
+  //res.send('Muchas gracias por contatar con nosotros '+email+' , será cocinado en breve.');
+  res.render('validar-contacto', {email:email, password:password})
 });
 
 /* GET paginas de ingredientes. */
